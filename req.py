b8c71616250cab85dfd6c1dcdfc6b83aa913341f
Eugenes <eugenes@kraken.kali>
2022-09-05 18:41:14 +0100

    adding storing

--- req.py
+++ req.py
@@ -19,7 +19,7 @@ setup = randint(1, int(P-1))
 SETUP = pow(G, setup, P)
 
 url6 = "http://127.0.0.1:5000/SetTree?tree="
-url7 = "http://127.0.0.1:5000/SetTreeSetup?setup="
+url7 = "https://pfe-art-blockchain.herokuapp.com/SetTree?tree="
 
 requests.get(url6+"?tree=156451,5654")
 
@@ -29,7 +29,7 @@ for i in range(1,11) :
     print("*"*40)
     print("number of keys : ", pow(2, i))
     start = time.time() 
-    keyss = requests.get(url4+str(pow(2, i))).json()
+    keyss = requests.get(url5+str(pow(2, i))).json()
     end = time.time()
     print("took", end - start)
 
@@ -46,18 +46,23 @@ for i in range(1,11) :
     PrivateTree = Tree_To_Array(TreeBasedGroupDiffieHellman(leafkeys, NumberOfKeys)) 
     pubtree = Public_Tree(PrivateTree)
     tree = ""
+    end = time.time()
+    ART_TIME = end- start
     for t in pubtree:
         tree =tree + str(t) +','
     params = {'tree': pubtree}
 
-    ers =requests.get(url6+tree).content
 
+    start = time.time()
+    ers =requests.get(url6+tree).content
     end = time.time()
-    ART_TIME = end- start
+    req = end-start
+    print("request took :", end-start)
+  
 
     print("[i] Number of groupe participants :" , NumberOfKeys)
     print("[!] Time took to request {} keys : {} s".format(NumberOfKeys, getting_keys_time))
     print("[i]ART tree creation Time         : ", ART_TIME)
-    print("[i]Totale Time :",getting_keys_time+ART_TIME)
+    print("[i]Totale Time :",getting_keys_time+ART_TIME+req)
    
 
